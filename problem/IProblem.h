4ce2e7f7714315bc156a5d7e36da28ffafe1c202
jashanj0tsingh <jshn.singh1@gmail.com>
2025-04-06 18:05:46 -0400

    defines the IProblem interface

---
+++ problem/IProblem.h
@@ -0,0 +1,19 @@
+//
+// Created by Jashanjot Singh on 2025-04-06.
+//
+
+#ifndef IPROBLEM_H
+#define IPROBLEM_H
+
+class IProblem {
+  public:
+    // `virtual` ensures that the correct subclass destrcutor runs when you
+    // delete through a pointer to `IProblem`
+    virtual ~IProblem() = default;
+
+    // virtual allows dynamic dispatch whereas pure (= 0) ensures that the
+    // function must be implemented by derived classes.
+    virtual void run() = 0;
+};
+
+#endif //IPROBLEM_H
