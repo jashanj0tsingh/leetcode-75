155b4d8d329c7e55b6ccff963997e4cae0a47218
jashanj0tsingh <jshn.singh1@gmail.com>
2025-04-06 21:51:22 -0400

    adds solution for two sum & CMakeLists.txt for arrays dir

---
+++ arrays/TwoSum.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <ostream>
+#include <vector>
+
+#include "problem/IProblem.h"
+//
+// Created by Jashanjot Singh on 2025-04-06.
+//
+
+class TwoSum : IProblem {
+public:
+    void run() override {
+        // input
+        const std::vector<int> nums= {2,7,11,15};
+        constexpr int target = 9;
+
+        const auto indices = twoSum(nums, target);
+        std::cout << "[" <<  indices[0] << "," << indices[1] << "]" << std::endl;
+    }
+
+private:
+    static std::vector<int> twoSum(const std::vector<int>& nums, const int target) {
+        std::unordered_map<int, int> map;
+        int index = 0;
+        for (const int num : nums) {
+            int diff = target - num;
+            if (map.find(diff) != map.end()) {
+                return {index, map[diff]};
+            }
+            map[num] = index;
+            ++index;
+        }
+        return {};
+    }
+};
